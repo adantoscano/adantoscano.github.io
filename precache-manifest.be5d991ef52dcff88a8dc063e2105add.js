814b3a4fd5b4d9da0709af068bce5f22b4ac6f84
Adán <adantoscano@gmail.com>
2021-04-27 01:51:46 +0200

    Updates

---
+++ precache-manifest.be5d991ef52dcff88a8dc063e2105add.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97022e8f05e7ff8219bbc2428ac217f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c2407305a963573d355",
+    "url": "/static/js/2.75a0ada5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.75a0ada5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39b78ac09bbb17bd1e81",
+    "url": "/static/js/main.9bcc5b97.chunk.js"
+  },
+  {
+    "revision": "ae471e450156301e8268",
+    "url": "/static/js/runtime-main.d1e812d3.js"
+  }
+]);
