d19d13ce56c14a4466b151b43bb942386c057160
Adán <adantoscano@gmail.com>
2021-04-27 01:39:53 +0200

    use react and bulma template

---
+++ src/components/structure/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer has-background-link">
+      <div className="content has-text-centered has-text-white">
+        <p>
+          Built with 
+          <a href="https://github.com/jcoelho93/personal-website" className="has-text-white">
+            <strong> personal-website</strong>
+          </a>
+          , Thanks!
+        </p>
+        <p>
+          <img
+            width="512"
+            height="96"
+            src="https://bulma.io/images/made-with-bulma--semiwhite.png"
+            className="bulma-image"
+            alt=""
+          />
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
